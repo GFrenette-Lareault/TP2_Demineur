a2fc3f3055d8b644d45b1c9a2f246a1d94c2c9ea
Francois Chantal <geniousmouton@gmail.com>
2014-10-07 16:19:03 -0400

    update de l'enum Difficult

--- TP2_Demineur/TP2_Demineur/src/ca/csf/TP2_Demineur/Difficulte.java
+++ TP2_Demineur/TP2_Demineur/src/ca/csf/TP2_Demineur/Difficulte.java
@@ -1,5 +1,27 @@
 package ca.csf.TP2_Demineur;
 
-enum Difficulte{
-	DEBUTANT,INTERMEDIAIRE,EXPERT
+enum Difficulte {
+	DEBUTANT(9, 9, 10), INTERMEDIAIRE(16, 16, 40), EXPERT(16, 30, 99);
+
+	private final int height;
+	private final int width;
+	private final int nbMine;
+
+	private Difficulte(int height, int width, int nbMine) {
+		this.height = height;
+		this.width = width;
+		this.nbMine = nbMine;
+	}
+
+	public int height() {
+		return height;
+	}
+	
+	public int width() {
+		return width;
+	}
+	
+	public int nbMine() {
+		return nbMine;
+	}
 }
