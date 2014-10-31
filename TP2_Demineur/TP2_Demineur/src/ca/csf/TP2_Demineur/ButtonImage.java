7e5a34acc15be8ee6bb562f3cd18eb48be1656af
Francois Chantal <geniousmouton@gmail.com>
2014-10-31 12:17:36 -0400

    reremodif

--- TP2_Demineur/TP2_Demineur/src/ca/csf/TP2_Demineur/ButtonImage.java
+++ TP2_Demineur/TP2_Demineur/src/ca/csf/TP2_Demineur/ButtonImage.java
@@ -1,7 +1,7 @@
 package ca.csf.TP2_Demineur;
 
 public enum ButtonImage {
-EMPTY(0,null), ONE(1,""), FLAG(9,""), QUESTIONMARK(10,"");
+EMPTY(0,null), ONE(1,""), FLAG(9,""), QUESTION_MARK(10,""), MINE_FLAG(11,""), MINE_NORMAL(12,"");
 	
 private final int index;
 private final String URL;
@@ -9,5 +9,13 @@ private final String URL;
 	private ButtonImage(int index,String URL){
 		this.index = index;
 		this.URL = URL;
+	}
+	
+	public int index(){
+		return index;
+	}
+	
+	public String URL(){
+		return URL;
 	}
 }
