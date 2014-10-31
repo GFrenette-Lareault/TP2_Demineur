6e1ef5b758b73281d70bdd33b3db72b3a860b7a3
Francois Chantal <geniousmouton@gmail.com>
2014-10-31 12:14:24 -0400

    modification a l'enum d'url

--- TP2_Demineur/TP2_Demineur/src/ca/csf/TP2_Demineur/ButtonImage.java
+++ TP2_Demineur/TP2_Demineur/src/ca/csf/TP2_Demineur/ButtonImage.java
@@ -1,7 +1,13 @@
 package ca.csf.TP2_Demineur;
 
 public enum ButtonImage {
+EMPTY(0,null), ONE(1,""), FLAG(9,""), QUESTIONMARK(10,"");
+	
+private final int index;
+private final String URL;
 
-	
-	
+	private ButtonImage(int index,String URL){
+		this.index = index;
+		this.URL = URL;
+	}
 }
